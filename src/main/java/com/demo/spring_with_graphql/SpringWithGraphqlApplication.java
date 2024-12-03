44f3887d7219c07d5d8cf60e9e0a9c0ff9a66106
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-12-03 17:30:20 +0300

    Initial commit

---
+++ src/main/java/com/demo/spring_with_graphql/SpringWithGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_with_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/spring_with_graphql/SpringWithGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_with_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
